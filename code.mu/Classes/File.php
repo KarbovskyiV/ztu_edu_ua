8212f4570b9d2476b5b91f6e9a1be09e2c77d37b
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-02-07 23:29:02 +0200

    add class File

---
+++ code.mu/Classes/File.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+include './Interfaces/iFile.php';
+
+class File implements iFile
+{
+
+    protected $filePath;
+
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
+    // шо тут робити з коментами? типу вони є в інтерфесі, логіка ж не змінюється методу, add return
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return realpath($this->filePath);
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function getDir(): string
+    {
+        return pathinfo($this->filePath, PATHINFO_DIRNAME);
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function getName(): string
+    {
+        return pathinfo($this->filePath, PATHINFO_FILENAME);
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function getExt(): string
+    {
+        return pathinfo($this->filePath, PATHINFO_EXTENSION);
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function getSize(): string
+    {
+        return $this->filePath . ': ' . filesize($this->filePath) . ' bytes.';
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function getText(): string
+    {
+        return file_get_contents($this->filePath);
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function setText($text): string
+    {
+        file_put_contents($this->filePath, $text);
+        return "$this->filePath with '$text' successfully recorded";
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function appendText($text): string
+    {
+        file_put_contents($this->filePath, $text . PHP_EOL, FILE_APPEND | LOCK_EX);
+        return "$this->filePath with '$text' successfully appended";
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function copy($copyPath): string
+    {
+        copy($this->filePath, $copyPath);
+        return "$this->filePath successfully copied into $copyPath";
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function delete(): string
+    {
+        unlink($this->filePath);
+        return "$this->filePath successfully deleted";
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function rename($newName): string
+    {
+        rename($this->filePath, $newName);
+        return "$this->filePath successfully renamed to $newName";
+    }
+
+    /**
+     * @inheritDoc
+     * @return string
+     */
+    public function replace($newPath): string
+    {
+        move_uploaded_file($this->filePath, $newPath);
+        return "$this->filePath successfully moved to $newPath";
+    }
+}
--- code.mu/index.php
+++ code.mu/index.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 include 'Classes/Date.php';
 include 'Classes/Interval.php';
 include 'Classes/Tag.php';
-
-$date = new Date('2025-12-31');
+include 'Classes/File.php';
 
 // Data
+//$date = new Date('2025-12-31');
+
 //echo $date->getYear();  // will display '2025'
 //echo $date->getMonth(); // will display '12'
 //echo $date->getDay();   // will display '31'
@@ -26,13 +27,38 @@ $date = new Date('2025-12-31');
 //echo $date;
 
 // Interval
-$date1 = new Date('2025-12-31');
-$date2 = new Date('2026-11-28');
-
-$interval = new Interval($date1, $date2);
+//$date1 = new Date('2025-12-31');
+//$date2 = new Date('2026-11-28');
+//
+//$interval = new Interval($date1, $date2);
 
 //echo $interval->toDays();   // выведет разницу в днях
 //echo $interval->toMonths(); // выведет разницу в месяцах
 //echo $interval->toYears();  // выведет разницу в годах
 
+// File
+$file = new File('text.txt');
 
+//echo $file->getPath();
+//echo $file->getDir();
+//echo $file->getName();
+//echo $file->getExt();
+//echo $file->getSize();
+//echo $file->getText();
+//echo $file->setText('text');
+//echo $file->appendText('tpt');
+//echo $file->copy('.test');
+//echo $file->delete();
+//echo $file->rename('text.txt');
+echo $file->replace('F:\OSPanel\domains\ztu.edu\code.mu\files');
+
+
+// Tag
+//$input = new Tag('input');
+//echo $input->open();
+//$tag = new Tag('div');
+//echo $tag->open() . 'text' . $tag->close();
+//$img = new Tag('img');
+//echo $img->open();
+//$header = new Tag('header');
+//echo $header->open() . 'Site header' . $header->close();
