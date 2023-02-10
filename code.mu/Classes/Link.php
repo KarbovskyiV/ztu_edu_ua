2b574f6ed966f23edcaf7125965b9e499d7186e2
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-02-10 11:28:05 +0200

    add Link class and menu

---
+++ code.mu/Classes/Link.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+const CLASS_ACTIVE = 'active';
+
+/**
+ * Class for work with link in HTML
+ */
+class Link extends Tag
+{
+    public function __construct()
+    {
+        $this->setAttr('href', '');
+
+        parent::__construct('a');
+    }
+
+    public function open(): string
+    {
+        $this->activateSelf();
+        return parent::open();
+    }
+
+    /**
+     * Checking if link is active
+     * @return void
+     */
+    private function activateSelf(): void
+    {
+        if ($this->getAttr('href') === $_SERVER['REQUEST_URI']) {
+            $this->addClass(CLASS_ACTIVE);
+        }
+    }
+}
