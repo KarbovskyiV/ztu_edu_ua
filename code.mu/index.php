f22977fde4ee0a558d4f8fa00943a62dd4ecf9b2
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-02-16 10:22:14 +0200

    autoload classes test

--- code.mu/index.php
+++ code.mu/index.php
@@ -12,3 +12,12 @@ $projectController = new \project\Controller();
 
 $marketCart = new \Market\Cart\Cart();
 $shopCart = new \Shop\Cart\Cart();
+
+spl_autoload_register();
+$obj1 = new Core\User;
+$obj2 = new Core\Admin\Controller;
+$obj3 = new Project\User\Data;
+
+var_dump($obj1);
+var_dump($obj2);
+var_dump($obj3);
